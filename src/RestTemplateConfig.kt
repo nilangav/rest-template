29a2f04b6a9a639ee5c9fd942f0b65656c918187
Nilanga Virajith <nilangavirajith.13@cse.mrt.ac.lk>
2023-05-29 13:08:23 +0800

    Create RestTemplateConfig.kt

---
+++ src/RestTemplateConfig.kt
@@ -0,0 +1,64 @@
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.*
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class RestTemplateConfig(
+    private val clientRegistrationRepository: ClientRegistrationRepository,
+    private val authorizedClientRepository: OAuth2AuthorizedClientRepository
+) {
+    @Value("\${oauth2.client.registrationId}")
+    private lateinit var registrationId: String
+
+    @Bean
+    fun authorizedClientManager(
+        clientRegistrationRepository: ClientRegistrationRepository,
+        authorizedClientRepository: OAuth2AuthorizedClientRepository
+    ): OAuth2AuthorizedClientManager {
+        return DefaultOAuth2AuthorizedClientManager(
+            clientRegistrationRepository, authorizedClientRepository
+        )
+    }
+
+    @Bean
+    fun restTemplate(authorizedClientManager: OAuth2AuthorizedClientManager): RestTemplate {
+        val restTemplate = RestTemplate(
+            BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory())
+        )
+        restTemplate.interceptors.add(ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution ->
+            val authentication = SecurityContextHolder.getContext().authentication
+            val clientRegistration = clientRegistrationRepository.findByRegistrationId(registrationId)
+            val authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(registrationId)
+                .principal(authentication)
+                .build()
+            val client = authorizedClientManager.authorize(authorizeRequest)
+            if (client != null) {
+                request.headers.setBearerAuth(client.accessToken.tokenValue)
+            }
+            execution.execute(request, body)
+        })
+
+        return restTemplate
+    }
+}
+
+
+// implementation("org.springframework.boot:spring-boot-starter-web")
+// 	implementation("org.springframework.boot:spring-boot-starter-security")
+// 	implementation("org.springframework.security:spring-security-oauth2-client")
+// 	implementation("org.springframework.security:spring-security-oauth2-jose")
+// 	implementation("org.springframework.security:spring-security-oauth2-jwt")
+// 	implementation("org.springframework.security:spring-security-config")
+// 	implementation("org.springframework.security:spring-security-data")
+// 	implementation("org.springframework.security:spring-security-web")
+// 	implementation("org.springframework.security:spring-security-core")
+// 	implementation ("org.springframework.boot:spring-boot-starter-oauth2-client")
